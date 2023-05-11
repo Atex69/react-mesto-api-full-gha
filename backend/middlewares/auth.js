51b60dfa62922eb37ef63a5d1aaa88c1ba0772db
Emil Bayanov <atex-69@mail.ru>
2023-05-11 17:03:35 +0300

    restart8

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,22 +1,24 @@
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/AuthError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new AuthError('Необходима авторизация'));
+    return (new AuthError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'yandex-praktikum');
+    payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'yandex-praktikum'}`);
+    req.user = payload;
   } catch (err) {
-    return next(new AuthError('Необходима авторизация'));
+    return (new AuthError('Необходима авторизация'));
   }
 
-  req.user = payload;
-  next();
+  return next();
 };
